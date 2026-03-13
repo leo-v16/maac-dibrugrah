b5b454aaa202de56da614b15acc2c65ef0b05129
leo-v16 <abhishekmazumder72@gmail.com>
2026-03-13 23:51:33 +0530

    Dynamic Dildo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["isomorphic-dompurify"],
+  serverExternalPackages: ["isomorphic-dompurify", "jsdom"],
   images: {
     remotePatterns: [
       {
